087be9ba64c69ec4963df8793c0c0f58ecfc865d
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2022-08-10 15:45:07 -0400

    fix arguments.update with weight loading

--- tools/detector_pretrain_net.py
+++ tools/detector_pretrain_net.py
@@ -78,7 +78,7 @@ def train(cfg, local_rank, distributed, logger, experiment):
         logger.info('finished instatntiating checkpointer')
         extra_checkpoint_data = checkpointer.load(cfg.MODEL.WEIGHT, update_schedule=cfg.SOLVER.UPDATE_SCHEDULE_DURING_LOAD)
         logger.info('finished loading extra checkpoint data')
-    arguments.update(extra_checkpoint_data)
+        arguments.update(extra_checkpoint_data)
 
     if cfg.MODEL.BACKBONE.CONV_BODY == 'VGG-16':
         vgg16_pretrain_strategy = cfg.MODEL.VGG.PRETRAIN_STRATEGY
